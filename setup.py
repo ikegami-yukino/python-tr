b3606f30eb1d5a1fb8322338d9cdbfbbc7c276dd
Yukino Ikegami <yknikgm@gmail.com>
2016-03-12 12:09:06 +0900

    Support Windows

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+from codecs import open
 import os
 import re
 from setuptools import setup
 
-with open(os.path.join('tr', '__init__.py'), 'r') as f:
+with open(os.path.join('tr', '__init__.py'), 'r', encoding='utf8') as f:
     version = re.compile(
         r".*__version__ = '(.*?)'", re.S).match(f.read()).group(1)
 
@@ -33,5 +34,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Topic :: Text Processing :: General'
         ],
-    long_description=open('README.rst').read() + '\n\n' + open('CHANGES.rst').read()
+    long_description='%s\n\n%s' % (open('README.rst', encoding='utf8').read(),
+                                   open('CHANGES.rst', encoding='utf8').read())
 )
